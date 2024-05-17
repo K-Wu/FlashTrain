742acf9fc44d741f8cad8b01d2b4fd2fc01b27b3
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-05-17 08:41:34 -0500

    rename flashtrain benchmark

---
+++ flexgen_benchmark/third_party/DeepSpeed/deepspeed/autotuning/tuner/__init__.py
@@ -0,0 +1,4 @@
+from .index_based_tuner import RandomTuner, GridSearchTuner
+
+# from .ga_tuner import GATuner
+from .model_based_tuner import ModelBasedTuner
---
+++ flexgen_benchmark/third_party/DeepSpeed/deepspeed/ops/transformer/inference/__init__.py
@@ -0,0 +1,5 @@
+from .config import DeepSpeedInferenceConfig
+from ....model_implementations.transformers.ds_transformer import (
+    DeepSpeedTransformerInference,
+)
+from .moe_inference import DeepSpeedMoEInferenceConfig, DeepSpeedMoEInference
