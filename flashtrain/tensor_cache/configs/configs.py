586597b1f5fb55ef298c1bf1e61821cf8d5d896f
K-Wu <kunwu2@illinois.edu>
2024-05-26 00:08:06 -0500

    [tensor cache] impl get_adapter()

--- flashtrain/tensor_cache/configs/configs.py
+++ flashtrain/tensor_cache/configs/configs.py
@@ -7,10 +7,38 @@ from ..adapters import (
     TorchMainMemoryIOAdapter,
     RevolverIOAdapter,
 )
+import torch
 
-IMPACT_HOSTNAMES_TO_CONFIGS = {
-    "bafs-01",
-    "kwu-csl227-99-CEntosREfugee",
+
+IMPACT_HOSTNAMES = {"bafs-01", "kwu-csl227-99-CEntosREfugee"}
+
+HOSTNAMES_TO_CONFIGS = {
+    "bafs-01": {
+        "adapter": [
+            [
+                (KvikioIOAdapter, {"path": "/scratch/impact/cache"}),
+                (KvikioIOAdapter, {"path": "/scratch/impact/cache"}),
+                (KvikioIOAdapter, {"path": "/scratch/impact/cache"}),
+            ],  # Rank 0
+            [
+                (KvikioIOAdapter, {"path": "/scratch/impact/cache"}),
+                (KvikioIOAdapter, {"path": "/scratch/impact/cache"}),
+                (KvikioIOAdapter, {"path": "/scratch/impact/cache"}),
+            ],  # Rank 1
+        ],
+    },
+    "kwu-csl227-99-CEntosREfugee": {
+        "adapter": [
+            [(TorchMainMemoryIOAdapter, {})],  # Rank 0
+            [(TorchMainMemoryIOAdapter, {})],  # Rank 1
+        ],
+    },
+    "ncsa-delta": {
+        "adapter": [
+            [(TorchMainMemoryIOAdapter, {})],  # Rank 0
+            [(TorchMainMemoryIOAdapter, {})],  # Rank 1
+        ],
+    },
 }
 
 
@@ -19,9 +47,28 @@ def get_hostname():
     if ".delta.ncsa.illinois.edu" in hostname:
         return "ncsa-delta"
     else:
-        if not hostname in IMPACT_HOSTNAMES_TO_CONFIGS:
+        if not hostname in IMPACT_HOSTNAMES:
             logger.warning(
                 "Hostname not known. Please add to IMPACT_HOSTNAMES in"
                 " flashtrain.tensor_cache.configs.utils."
             )
         return hostname
+
+
+def _get_rank() -> int:
+    if torch.distributed.is_initialized():
+        return torch.distributed.get_rank()
+    else:
+        return 0
+
+
+def get_adapter():
+    hostname = get_hostname()
+    rank = _get_rank()
+    class_and_kwargs = HOSTNAMES_TO_CONFIGS[hostname]["adapter"][rank]
+    if len(class_and_kwargs) == 1:
+        return class_and_kwargs[0][0](**class_and_kwargs[0][1])
+    else:
+        return RevolverIOAdapter(
+            adapters=[c[0](**c[1]) for c in class_and_kwargs]
+        )
