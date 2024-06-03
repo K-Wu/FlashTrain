1e8420c462cc93793e8e5c1428bc2639be5d55f6
K-Wu <kunwu2@illinois.edu>
2024-06-03 03:36:22 -0500

    add cleanup script

---
+++ flashtrain/tensor_cache/configs/clear_stored_tensors_bafs.sh
@@ -0,0 +1,8 @@
+ls /mnt/raid0/kunwu2/FlashTrain_temp/adapter0/bafs-01* | xargs rm
+ls /mnt/raid0/kunwu2/FlashTrain_temp/adapter1/bafs-01* | xargs rm
+ls /mnt/raid0/kunwu2/FlashTrain_temp/adapter2/bafs-01* | xargs rm
+ls /mnt/nvme14/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
+ls /mnt/md1/kunwu2/FlashTrain_temp/adapter0/bafs-01* | xargs rm
+ls /mnt/md1/kunwu2/FlashTrain_temp/adapter1/bafs-01* | xargs rm
+ls /mnt/nvme7/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
+ls /mnt/nvme17/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
