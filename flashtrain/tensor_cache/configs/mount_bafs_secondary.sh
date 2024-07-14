1f9fa686e071bd49d3e3279cb8df1cb99f63ecd6
K-Wu <kunwu2@illinois.edu>
2024-07-14 02:02:27 -0500

    aux script

--- flashtrain/tensor_cache/configs/mount_bafs_secondary.sh
+++ flashtrain/tensor_cache/configs/mount_bafs_secondary.sh
@@ -3,7 +3,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${DIR}/_mount_all_func.sh"
 
 # Mount /dev/md1 to /mnt/md1 if it is not already mounted.
-mount_if_not /dev/md1 /mnt/md1
+# mount_if_not /dev/md1 /mnt/md1
 
 # The following should be mounted by default
 mount_if_not /dev/md0 /mnt/raid0
