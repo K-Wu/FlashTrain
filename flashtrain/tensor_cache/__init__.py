c0b32745b009585f7039f991c63c09356e440b2a
K-Wu <kunwu2@illinois.edu>
2024-07-18 20:03:19 -0500

    add utils to print memory break down

--- flashtrain/tensor_cache/__init__.py
+++ flashtrain/tensor_cache/__init__.py
@@ -1,6 +1,8 @@
 # Adapted from megatron/global_vars.py
+from typing import Optional
 
 __GLOBAL_TENSOR_CACHE = None
+_GLOBAL_MEMORY_USE_STATS_RECORDER: Optional[dict[str, int]] = None
 
 
 def _ensure_var_is_not_initialized(var, name):
@@ -22,3 +24,20 @@ def set_tensor_cache(tensor_cache):
 def get_tensor_cache():
     _ensure_var_is_initialized(__GLOBAL_TENSOR_CACHE, "tensor cache")
     return __GLOBAL_TENSOR_CACHE
+
+
+def get_memory_use_stats_recorder():
+    """Return timers."""
+    _ensure_var_is_initialized(
+        _GLOBAL_MEMORY_USE_STATS_RECORDER, "memory use stats recorder"
+    )
+    return _GLOBAL_MEMORY_USE_STATS_RECORDER
+
+
+def init_memory_use_stats_recorder():
+    """Initialize timers."""
+    global _GLOBAL_MEMORY_USE_STATS_RECORDER
+    _ensure_var_is_not_initialized(
+        _GLOBAL_MEMORY_USE_STATS_RECORDER, "memory use stats recorder"
+    )
+    _GLOBAL_MEMORY_USE_STATS_RECORDER = dict()
