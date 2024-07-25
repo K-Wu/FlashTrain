9868099c879f1a3cc509e88008fb85ac01b72c57
K-Wu <kunwu2@illinois.edu>
2024-07-24 23:16:43 -0500

    test_malloc_hook

--- flashtrain/tests/test_malloc_hook.py
+++ flashtrain/tests/test_malloc_hook.py
@@ -9,6 +9,7 @@ libcuda_hook = ctypes.CDLL(
     mode=ctypes.RTLD_GLOBAL,
 )
 libcuda_hook.set_verbose(True)
+libcuda_hook.set_enable_cufile_registration(True)
 torch.randn(15).cuda()
 # Call empty_cache to trigger cudaFree and force calling cudaMallow in the next tensor creation call.
 torch.cuda.empty_cache()
