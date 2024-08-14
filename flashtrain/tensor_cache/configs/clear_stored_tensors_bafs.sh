b37cd65f748e1b91714ea1505ac020c4667f5005
K-Wu <kunwu2@illinois.edu>
2024-08-13 20:11:57 -0500

    aux scripts

--- flashtrain/tensor_cache/configs/clear_stored_tensors_bafs.sh
+++ flashtrain/tensor_cache/configs/clear_stored_tensors_bafs.sh
@@ -2,3 +2,5 @@ ls /mnt/md2/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
 ls /mnt/md3/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
 ls /mnt/md4/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
 ls /mnt/md5/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
+ls /mnt/md6/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
+ls /mnt/md7/kunwu2/FlashTrain_temp/bafs-01* | xargs rm
--- flashtrain/tensor_cache/configs/samsung/3_mount_bafs.sh
+++ flashtrain/tensor_cache/configs/samsung/3_mount_bafs.sh
@@ -1,6 +1,6 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-source "${DIR}/_mount_all_func.sh"
+source "${DIR}/../_mount_all_func.sh"
 
 # Mount the following disks if it is not already mounted.
 mount_if_not /dev/md6 /mnt/md6
